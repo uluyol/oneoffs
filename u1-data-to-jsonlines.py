67b08065a1617c705980a908854734d5b4d4878d
Muhammed Uluyol <uluyol0@gmail.com>
2018-06-07 13:58:44 -0700

    parallelize and use cython for more speed (still slow)

--- u1-data-to-jsonlines.py
+++ u1-data-to-jsonlines.py
@@ -8,124 +8,91 @@
 # zcat ubuntuone_trace.gz | ./u1-data-to-jsonlines.py | gzip > ubuntuone_json.gz
 
 import ast
-import copy
 import csv
-import json
 import sys
 
-obj = {
-	"T": None,
-	"addr": None,
-	"caps": None,
-	"client_metadata": None,
-	"current_gen": None,
-	"ext": None,
-	"failed": None,
-	"free_bytes": None,
-	"from_gen": None,
-	"hash": None,
-	"level": None,
-	"logfile_id": None,
-	"method": None,
-	"mime": None,
-	"msg": None,
-	"node_id": None,
-	"nodes": None,
-	"pid": None,
-	"req_id": None,
-	"req_t": None,
-	"root": None,
-	"server": None,
-	"shared_by": None,
-	"shared_to": None,
-	"shares": None,
-	"sid": None,
-	"size": None,
-	"time": None,
-	"tstamp": None,
-	"type": None,
-	"udfs": None,
-	"user_id": None,
-	"user": None,
-	"vol_id": None,
-}
+from concurrent import futures
 
-fix_rules = [
-	("client_metadata", dict(), dict),
-	("ext", "", str),
-	("mime", "", str),
-	("size", -1, int),
-	("shares", -1, int),
-	("time", -1.0, float),
-	("pid", -1, int),
-	("free_bytes", -1, int),
-	("req_id", -1, int),
-	("hash", -1, int),
-	("node_id", -1, int),
-	("user_id", -1, int),
-	("root", -1, int),
-	("udfs", -1, int),
-	("vol_id", -1, int),
-	("current_gen", -1, int),
-	("from_gen", -1, int),
-	("nodes", -1, int),
-	("user", -1, int),
-	("shared_by", -1, int),
-	("shared_to", -1, int),
-]
+import simplejson as json
 
-for row in csv.reader(sys.stdin):	
-	obj["T"] = row[0]
-	obj["addr"] = row[1]
-	obj["caps"] = row[2]
-	obj["client_metadata"] = row[3]
-	obj["current_gen"] = row[4]
-	obj["ext"] = row[5]
-	obj["failed"] = row[6]
-	obj["free_bytes"] = row[7]
-	obj["from_gen"] = row[8]
-	obj["hash"] = row[9]
-	obj["level"] = row[10]
-	obj["logfile_id"] = row[11]
-	obj["method"] = row[12]
-	obj["mime"] = row[13]
-	obj["msg"] = row[14]
-	obj["node_id"] = row[15]
-	obj["nodes"] = row[16]
-	obj["pid"] = row[17]
-	obj["req_id"] = row[18]
-	obj["req_t"] = row[19]
-	obj["root"] = row[20]
-	obj["server"] = row[21]
-	obj["shared_by"] = row[22]
-	obj["shared_to"] = row[23]
-	obj["shares"] = row[24]
-	obj["sid"] = row[25]
-	obj["size"] = row[26]
-	obj["time"] = row[27]
-	obj["tstamp"] = row[28]
-	obj["type"] = row[29]
-	obj["udfs"] = row[30]
-	obj["user_id"] = row[31]
-	obj["user"] = row[32]
-	obj["vol_id"] = row[33]
+import pyximport; pyximport.install()
+import u1_fix_data_ext
 
-	for key, default, type in fix_rules:
-		if obj[key]:
-			obj[key] = type(ast.literal_eval(obj[key]))
-		else:
-			obj[key] = copy.copy(default)
-	if obj["caps"].startswith("frozenset("):
-		t = obj["caps"][len("frozenset("):-1]
-		obj["caps"] = ast.literal_eval(t)
-		obj["caps"].sort()
-	elif obj["caps"] == "":
-		obj["caps"] = []
-	else:
-		raise ValueError("caps is not empty and is not frozenset: " + obj["caps"])
+def tojson(lines):
+	obj = {
+		"T": None,
+		"addr": None,
+		"caps": None,
+		"client_metadata": None,
+		"current_gen": None,
+		"ext": None,
+		"failed": None,
+		"free_bytes": None,
+		"from_gen": None,
+		"hash": None,
+		"level": None,
+		"logfile_id": None,
+		"method": None,
+		"mime": None,
+		"msg": None,
+		"node_id": None,
+		"nodes": None,
+		"pid": None,
+		"req_id": None,
+		"req_t": None,
+		"root": None,
+		"server": None,
+		"shared_by": None,
+		"shared_to": None,
+		"shares": None,
+		"sid": None,
+		"size": None,
+		"time": None,
+		"tstamp": None,
+		"type": None,
+		"udfs": None,
+		"user_id": None,
+		"user": None,
+		"vol_id": None,
+	}
 
-	try:
-		sys.stdout.write(json.dumps(obj, sort_keys=True))
-		sys.stdout.write("\n")
-	except BrokenPipeError:
-		break
+	out = []
+	for row in csv.reader(lines):	
+		u1_fix_data_ext.load_and_fix(obj, row)
+		out.append(json.dumps(obj, sort_keys=True) + "\n")
+	return out
+
+def chunklines(lines, csize):
+	ll = []
+	l = []
+	pos = 0
+	while pos < len(lines):
+		for i in range(csize):
+			l.append(lines[pos])
+			pos += 1
+			if pos >= len(lines):
+				break
+		ll.append(l)
+		l = []
+	return ll
+
+with futures.ProcessPoolExecutor() as exec:
+	done = False
+	while not done:
+		lines = []
+		n = 0
+		for line in sys.stdin:
+			lines.append(line)
+			n += 1
+			if n > 50000:
+				break
+
+		chunks = chunklines(lines, 500)
+
+		jll = exec.map(tojson, chunks)
+		for jl in jll:
+			for l in jl:
+				try:
+					sys.stdout.write(l)
+				except BrokenPipeError:
+					sys.exit(0)
---
+++ u1_fix_data_ext.pyx
@@ -0,0 +1,101 @@
+import ast
+
+fix_toint = [
+	"size",
+	"shares",
+	"pid",
+	"free_bytes",
+	"req_id",
+	"hash",
+	"node_id",
+	"user_id",
+	"root",
+	"udfs",
+	"vol_id",
+	"current_gen",
+	"from_gen",
+	"nodes",
+	"user",
+	"shared_by",
+	"shared_to",
+]
+
+cdef object fix_float(str t):
+	if t:
+		return str(t)
+	return -1.0
+
+cdef str fix_str(str t):
+	if t:
+		if t == "None":
+			return ""
+		return ast.literal_eval(t)
+	return ""
+
+cdef object fix_dict(str t):
+	if t:
+		if t == "None":
+			return {}
+		return dict(ast.literal_eval(t))
+	else:
+		return {}
+
+def load_and_fix(obj, row):
+	obj["T"] = row[0]
+	obj["addr"] = row[1]
+	obj["caps"] = row[2]
+	obj["client_metadata"] = row[3]
+	obj["current_gen"] = row[4]
+	obj["ext"] = row[5]
+	obj["failed"] = row[6]
+	obj["free_bytes"] = row[7]
+	obj["from_gen"] = row[8]
+	obj["hash"] = row[9]
+	obj["level"] = row[10]
+	obj["logfile_id"] = row[11]
+	obj["method"] = row[12]
+	obj["mime"] = row[13]
+	obj["msg"] = row[14]
+	obj["node_id"] = row[15]
+	obj["nodes"] = row[16]
+	obj["pid"] = row[17]
+	obj["req_id"] = row[18]
+	obj["req_t"] = row[19]
+	obj["root"] = row[20]
+	obj["server"] = row[21]
+	obj["shared_by"] = row[22]
+	obj["shared_to"] = row[23]
+	obj["shares"] = row[24]
+	obj["sid"] = row[25]
+	obj["size"] = row[26]
+	obj["time"] = row[27]
+	obj["tstamp"] = row[28]
+	obj["type"] = row[29]
+	obj["udfs"] = row[30]
+	obj["user_id"] = row[31]
+	obj["user"] = row[32]
+	obj["vol_id"] = row[33]
+
+	cdef str key, t
+	for key in fix_toint:
+		t = obj[key]
+		if t:
+			obj[key] = int(t)
+		else:
+			obj[key] = -1
+
+	obj["time"] = fix_float(obj["time"])
+
+	obj["ext"] = fix_str(obj["ext"])
+	obj["mime"] = fix_str(obj["mime"])
+
+	obj["client_metadata"] = fix_dict(obj["client_metadata"])
+
+	if obj["caps"].startswith("frozenset("):
+		t = obj["caps"][len("frozenset("):-1]
+		obj["caps"] = ast.literal_eval(t)
+		obj["caps"].sort()
+	elif obj["caps"] == "":
+		obj["caps"] = []
+	else:
+		raise ValueError("caps is not empty and is not frozenset: " + obj["caps"])
